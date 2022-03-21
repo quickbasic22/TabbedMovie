c5aa2be9c7d39d8d8f3dbfcbe90f16cd425c15c6
David Morrow <quickbasic44@outlook.com>
2022-03-21 16:31:00 -0400

    Add project files.

---
+++ TabbedMovie/TabbedMovie/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using TabbedMovie.ViewModels;
+using TabbedMovie.Views;
+using Xamarin.Forms;
+
+namespace TabbedMovie
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ TabbedMovie/TabbedMovie/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using TabbedMovie.ViewModels;
+using Xamarin.Forms;
+
+namespace TabbedMovie.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
